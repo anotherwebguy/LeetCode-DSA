aa7fedef531495de234a7331755ed4f6b24a1751
Mohit singh <mohitrajputms123@gmail.com>
2022-09-22 23:28:20 +0530

    Create Max Value of Equation.cpp

---
+++ Arrays/Max Value of Equation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,int>> pq;
+        pq.push({points[0][1]-points[0][0],points[0][0]});
+        int ans = INT_MIN;
+        for(int i=1;i<points.size();i++){
+            auto x = points[i];
+            while(!pq.empty() && (x[0]-pq.top().second > k)) pq.pop();
+            if(!pq.empty()){
+                ans = max(ans,x[0]+x[1]+pq.top().first);
+            }
+            pq.push({x[1]-x[0],x[0]});
+        }
+        return ans;
+    }
+};

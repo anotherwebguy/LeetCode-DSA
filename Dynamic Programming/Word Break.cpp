b04dfe04f035ac5dd69b565232597fc58ac66825
Mohit singh <mohitrajputms123@gmail.com>
2022-09-24 22:56:18 +0530

    Update Word Break.cpp

--- Dynamic Programming/Word Break.cpp
+++ Dynamic Programming/Word Break.cpp
@@ -19,3 +19,45 @@ public:
 };
 
 //method 2
+class Solution {
+public:
+    bool recursive(int ind,string s, vector<string> &wordDict,vector<int> &dp){
+        if(s.size()==ind) return true;
+        if(dp[ind]!=-1) return dp[ind];
+        for(int i=ind;i<s.size();i++){
+            string k = s.substr(ind,i-ind+1);
+            if(find(wordDict.begin(),wordDict.end(),k)!=wordDict.end() && 
+                 recursive(i+1,s,wordDict,dp)){
+                return dp[ind]=true;
+            }
+        }
+        return dp[ind]=false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> dp(s.size(),-1);
+        return recursive(0,s,wordDict,dp);      
+    }
+};
+
+//method 3
+class Solution {
+public:
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> dp(s.size()+1,0);
+        set<string> dict(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        dp[n]=1;
+        for(int ind = n-1; ind >= 0; ind--) {
+            for(int i = ind; i < s.length(); i++) {
+                string cur = s.substr(ind, i-ind+1);
+                if(dict.find(cur) != dict.end() && dp[i+1]) {
+                    dp[ind] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[0];      
+    }
+};
